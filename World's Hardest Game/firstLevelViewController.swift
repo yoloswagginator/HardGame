39795f2a67699bfc957f5eea3b581ec80b576058
student3 <student3@jh-mbair-158-16.d214.org>
2016-05-02 14:23:48 -0500

    initial commit

---
+++ World's Hardest Game/firstLevelViewController.swift
@@ -0,0 +1,29 @@
+//
+//  firstLevelViewController.swift
+//  World's Hardest Game
+//
+//  Created by student3 on 4/29/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class firstLevelViewController: UIViewController {
+
+    @IBOutlet weak var startPlatform: UIImageView!
+    @IBOutlet weak var mainPlatform: UIImageView!
+    @IBOutlet weak var endPlatform: UIImageView!
+    @IBOutlet weak var playBall: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+   
+
+    
+}
