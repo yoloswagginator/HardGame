3a9f004996a031232ee27b7743f87e917b7824b3
student3 <student3@jh-mbair-158-16.d214.org>
2016-05-25 14:39:15 -0500

    gitruin is tubing my life

---
+++ World's Hardest Game/InstructionsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InstructionsViewController.swift
+//  World's Hardest Game
+//
+//  Created by student3 on 5/25/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class InstructionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+    @IBAction func dismiss(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+}
